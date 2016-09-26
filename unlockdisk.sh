c540143db3d19a0a29c54cbe376f5d453a558d48
eyedeekay <eyedeekay@i2pmail.org>
2016-09-26 06:39:33 +0000

    OK that will go ahead and loop through the contents of a wordlist trying passwords to an encrypted disk.

---
+++ unlockdisk.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+WORDLIST=$(cat ../wordlist.txt)
+mkdir mntScss
+for KEY in $WORDLIST; do
+	echo -n "$KEY" | sudo cryptsetup --key-file="-" luksOpen /dev/sda5 theDisk && sudo mount /dev/sda5 mntScss && echo "$KEY" > ../word.txt
+done
